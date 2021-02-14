d33d49971ce9ae85c606d8c0d4383b88fba4011b
pe200012 <1326263755@qq.com>
2021-02-14 16:08:44 +0800

    game loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import           Control.Monad                  ( join )
@@ -11,9 +13,15 @@ import           Data.Array                     ( (!)
                                                 , indices
                                                 , listArray
                                                 )
-import           Data.Maybe                     ( isJust
+import           Data.List                      ( dropWhileEnd )
+import           Data.Maybe                     ( fromMaybe
+                                                , isJust
                                                 , listToMaybe
                                                 )
+import           Data.Text.Format               ( format )
+import qualified Data.Text.Lazy.IO             as T
+import           System.IO
+import           Text.Read                      ( readMaybe )
 
 data Symbol = O | X deriving (Show, Eq)
 newtype Board = Board { unBoard :: Array (Int, Int) (Maybe Symbol) } deriving Show
@@ -38,6 +46,9 @@ prettyprintBoard (Board b) = foldr
     len     = 1 + snd (snd (bounds b))
     vBorder = '\n' : take (2 * len - 1) (concat (repeat "-█")) ++ "\n"
 
+displayBoard :: Board -> IO ()
+displayBoard = putStrLn . prettyprintBoard
+
 draw :: (Int, Int) -> Symbol -> Board -> Board
 draw i s (Board b) | i `notElem` indices b = Board b
                    | isJust (b ! i)        = Board b
@@ -47,13 +58,14 @@ draws :: [((Int, Int), Symbol)] -> Board -> Board
 draws = flip (foldr (uncurry draw))
 
 check :: Board -> Maybe Symbol
-check (Board b) = join $ listToMaybe $ concat (filter (\x -> length x >= 3 && allTheSame x && isJust (head x)) (fmap (b !) <$> ([column, row, dig1, dig2] <*> indices b)))
+check (Board b) = join $ listToMaybe $ concat
+    (filter (\x -> length x >= 3 && allTheSame x && isJust (head x)) (fmap (b !) <$> ([column, row, dig1, dig2] <*> indices b)))
   where
     translate (r, c) = (c, upperBound - r)
-    column (_, c0) = [(r, c0) | r <- [0..upperBound]]
-    row    (r0, _) = [(r0, c) | c <- [0..upperBound]]
-    dig1   (r0, c0) = [(r, c) | r <- [0..upperBound], c <- [0..upperBound], slash (translate (r0, c0)) (translate (r, c)) ]
-    dig2   (r0, c0) = [(r, c) | r <- [0..upperBound], c <- [0..upperBound], backslash (translate (r0, c0)) (translate (r, c)) ]
+    column (_, c0) = [ (r, c0) | r <- [0 .. upperBound] ]
+    row (r0, _) = [ (r0, c) | c <- [0 .. upperBound] ]
+    dig1 (r0, c0) = [ (r, c) | r <- [0 .. upperBound], c <- [0 .. upperBound], slash (translate (r0, c0)) (translate (r, c)) ]
+    dig2 (r0, c0) = [ (r, c) | r <- [0 .. upperBound], c <- [0 .. upperBound], backslash (translate (r0, c0)) (translate (r, c)) ]
     allTheSame :: Eq a => [a] -> Bool
     allTheSame = all . ((==) . head) <*> tail
     slash (x0, y0) (x, y) = y == (-x) + b where b = x0 + y0
@@ -61,4 +73,27 @@ check (Board b) = join $ listToMaybe $ concat (filter (\x -> length x >= 3 && al
     upperBound = fst (snd (bounds b))
 
 main :: IO ()
-main = pure ()
+main = do
+    c <- getHand
+    go c (board 3)
+  where
+    go s g = do
+        displayBoard g
+        hFlush stdout
+        case check g of
+            Nothing -> do
+                p <- getPos
+                go (next s) (draw p s g)
+            Just s' -> T.putStrLn (format "{} wins!" [show s'])
+    next O = X
+    next X = O
+    getPos :: IO (Int, Int)
+    getPos  = maybe getPos return . readMaybe =<< getLine
+    getHand = do
+        putStr "Which one first(O/X)?"
+        hFlush stdout
+        answer <- getLine
+        case dropWhile (== ' ') $ dropWhileEnd (== ' ') answer of
+            "O" -> return O
+            "X" -> return X
+            _   -> getHand
